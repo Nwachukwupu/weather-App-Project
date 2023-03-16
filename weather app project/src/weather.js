9b6c50acd3620a6e7a06a358a0dd1f958e2e0137
Nwachukwupu <120318967+Nwachukwupu@users.noreply.github.com>
2023-03-16 13:28:24 +0100

    updated javascript  and  html

--- weather app project/src/weather.js
+++ weather app project/src/weather.js
@@ -1,29 +1,37 @@
 function formatDate(timestamp) {
-    let date = newDate(timestamp);
+    let date = new Date(timestamp);
     let hours = date.getHours();
+    if (hours < 0) {
+        hours = `0${hours}`;
+    }
     let minutes = date.getMinutes();
+    if (minutes < 0) {
+        minutes = `0${minutes}`;
+    }
     let seconds = date.getSeconds();
-    let day = date.getDay();
-    
-
-    return "Friday 5:00";
+    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "saturday", "Sunday"]
+    let day = days [date.getDay()];
+    return `${day} ${hours}:${minutes}`;
 }
 
 
-function displayTemperature(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    let cityElement = document.querySelector("#city");
-    let descriptionElement = document.querySelector("#description");
-    let humidityElement = document.querySelector("#humidity");
-    let windElement = document.querySelector("#wind");
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
-    cityElement.innerHTML = response.data.name;
-    descriptionElement.innerHTML = response.data.weather[0].description;
-    humidityElement.innerHTML = response.data.main.humidity;
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-}
+    function displayTemperature(response) {
+        let temperatureElement = document.querySelector("#temperature");
+        let cityElement = document.querySelector("#city");
+        let descriptionElement = document.querySelector("#description");
+        let humidityElement = document.querySelector("#humidity");
+        let windElement = document.querySelector("#wind");
+        let dateElement = document.querySelector("#date");
 
-let apiKey = "d25df42b10f9cecb0836d249c9158e2b";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
- 
-axios.get(apiUrl).then(displayTemperature);
+        temperatureElement.innerHTML = Math.round(response.data.main.temp);
+        cityElement.innerHTML = response.data.name;
+        descriptionElement.innerHTML = response.data.weather[0].description;
+        humidityElement.innerHTML = response.data.main.humidity;
+        windElement.innerHTML = Math.round(response.data.wind.speed);
+        dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    }
+
+    let apiKey = "d25df42b10f9cecb0836d249c9158e2b";
+    let city = "owerri";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+ axios.get(apiUrl).then(displayTemperature);
