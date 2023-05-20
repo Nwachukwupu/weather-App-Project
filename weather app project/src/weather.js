2baddae8dc4620839bb19c2b8851d0cfa59a3b12
Nwachukwupu <120318967+Nwachukwupu@users.noreply.github.com>
2023-05-20 10:06:00 +0100

    javascript and html add

--- weather app project/src/weather.js
+++ weather app project/src/weather.js
@@ -14,7 +14,32 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(){
+   let forecastElement= document.querySelector = ("#forecast");
 
+   let forecastHTML = "";
+   forecastHTML = `
+   <div class="weather-forecast" id="forecast">
+   <div class="row">
+       <div class="col-2">
+           <div class="weather-forecast-date" >
+               Thurs
+           </div>
+           <img src="" alt="" width="">
+           <div class="weather-forecast-temperature">
+              <span class="weather-forecast-temperature-max">
+               18
+              </span>  <span class="weather-forecast-temperature-min">
+                12
+              </span>
+           </div>
+       </div>
+   </div>
+</div>
+`;
+   forecastElement.innerHTML = forecastHTML;
+
+}
     function displayTemperature(response) {
         let temperatureElement = document.querySelector("#temperature");
         let cityElement = document.querySelector("#city");
@@ -58,3 +83,9 @@ form.addEventListener("submit", handlesubmit);
 
 let fahrenheitLink = document.querySelector("#fahreheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-Link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("New York");
+displayForecast();
