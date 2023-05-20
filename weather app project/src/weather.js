d3bebd513114931b0c4528f69ed16a14c4661dc5
Nwachukwupu <120318967+Nwachukwupu@users.noreply.github.com>
2023-05-20 20:24:24 +0100

    forecast HTML added to javascript

--- weather app project/src/weather.js
+++ weather app project/src/weather.js
@@ -17,27 +17,35 @@ function formatDate(timestamp) {
 function displayForecast(){
    let forecastElement= document.querySelector = ("#forecast");
 
-   let forecastHTML = "";
-   forecastHTML = `
-   <div class="weather-forecast" id="forecast">
-   <div class="row">
-       <div class="col-2">
-           <div class="weather-forecast-date" >
-               Thurs
-           </div>
-           <img src="" alt="" width="">
-           <div class="weather-forecast-temperature">
-              <span class="weather-forecast-temperature-max">
-               18
-              </span>  <span class="weather-forecast-temperature-min">
-                12
-              </span>
-           </div>
-       </div>
-   </div>
-</div>
-`;
-   forecastElement.innerHTML = forecastHTML;
+   function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+  
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+  
+    let forecastHTML = `<div class="row">`;
+    days.forEach(function (day) {
+      forecastHTML =
+        forecastHTML +
+        `
+        <div class="col-2">
+          <div class="weather-forecast-date">${day}</div>
+          <img
+            src="http://openweathermap.org/img/wn/50d@2x.png"
+            alt=""
+            width="42"
+          />
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max"> 18° </span>
+            <span class="weather-forecast-temperature-min"> 12° </span>
+          </div>
+        </div>
+    `;
+    });
+  
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+    console.log(forecastHTML);
+  }
 
 }
     function displayTemperature(response) {
